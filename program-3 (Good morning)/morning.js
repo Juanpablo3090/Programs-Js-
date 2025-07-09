00c80a5d5e4636f53f93ddc6f9d6475f23928e4a
Rainthee34 <juanpabloosorio824@gmail.com>
2025-07-09 07:27:46 -0500

    Add files via upload

---
+++ program-3 (Good morning)/morning.js
@@ -0,0 +1,10 @@
+let hour = 7;
+
+if (hour < 12){
+    console.log("Good morning, you must have brakfast now");
+}
+
+else {
+    console.log("You'r late, you better be responsable or you'r not gonna have anything to eat next month.")
+
+}
