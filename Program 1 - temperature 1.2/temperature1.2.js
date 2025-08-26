2992c8ee583eb48970b7d5c967360820f0340244
Rainthee34 <juanpabloosorio824@gmail.com>
2025-08-26 13:44:59 -0500

    Add files via upload

---
+++ Program 1 - temperature 1.2/temperature1.2.js
@@ -0,0 +1,18 @@
+// FIRST VARIABLE (KELVIN)
+let kelvin = 294;
+// SECOND VARIABLE (CELSIUS)
+let celsius = kelvin - 273;
+// THIRD VARIABLE (FARENHEIT)
+let fahrenheit = celsius * (9/5) + 32;
+// TURNING A DECIMAL NUMBER INTO A INT
+fahrenheit = Math.floor(fahrenheit);
+// TURN CELSIUS INTO FAHRENHEIT
+let newton = celsius * (33/100);
+
+newton = Math.floor(newton);
+
+// PRINTING RESULTS USING CONSOLE LOG
+console.log(`The temperature  is ${fahrenheit} degrees fahrenheit`);
+
+console.log(`${newton} newtons`)
+
